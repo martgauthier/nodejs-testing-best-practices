2288167883891396900cbccf17af0a1e39b2d61f
Gauthier <gauthiermartin.public@gmail.com>
2025-12-02 10:19:15 +0100

    daqwsd

--- jest.config.js
+++ jest.config.js
@@ -5,7 +5,7 @@ module.exports = {
     '!**/mocha/**',
     '!**/playground/**',
     '!**/*test-helper*',
-    '!**/*anti-pattern*', // Uncomment this only when you want to inspect the consequences of anti-patterns
+    '!**/*anti-pattern*', // dqsdqsd this only when you want to inspect the consequences of anti-patterns
     '!**/*performance*', //Uncomment this only when you want to inspect the performance of tests
   ],
   collectCoverage: false,
